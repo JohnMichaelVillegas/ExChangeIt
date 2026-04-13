1960464cd878bce6d15c41e15234e8b9be6bee6c
JohnMichaelVillegas <johnmichael.villegas@benilde.edu.ph>
2026-04-13 16:30:13 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ExChangeIt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
